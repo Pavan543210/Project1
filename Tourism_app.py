fa6b59d79e6718e4d8cfa9a7053960ae1f6155d2
Pavan543210 <135861719+Pavan543210@users.noreply.github.com>
2024-05-13 23:11:35 +0530

    Update Tourism_app.py

--- Tourism_app.py
+++ Tourism_app.py
@@ -14,33 +14,26 @@ image6 = "./image6.jpg"
 image7 = "./image7.jpg"
 image8 = "./image8.jpg"
 
-# Resize images to a specific dimension (400x300 pixels)
-def resize_image(image_path, output_size=(400, 300)):
+# Resize images to a larger size
+def resize_image(image_path, output_size=(600, 450)):
     image = Image.open(image_path)
     image = image.resize(output_size)
     return image
 
 # Display Images
-col1, col2, col3, col4 = st.columns(4)
+col1, col2 = st.columns(2)
 with col1:
     st.image(resize_image(image1), use_column_width=True, caption='Image 1')
+    st.image(resize_image(image3), use_column_width=True, caption='Image 3')
+    st.image(resize_image(image5), use_column_width=True, caption='Image 5')
+    st.image(resize_image(image7), use_column_width=True, caption='Image 7')
+
 with col2:
     st.image(resize_image(image2), use_column_width=True, caption='Image 2')
-with col3:
-    st.image(resize_image(image3), use_column_width=True, caption='Image 3')
-with col4:
     st.image(resize_image(image4), use_column_width=True, caption='Image 4')
-
-col5, col6, col7, col8 = st.columns(4)
-with col5:
-    st.image(resize_image(image5), use_column_width=True, caption='Image 5')
-with col6:
     st.image(resize_image(image6), use_column_width=True, caption='Image 6')
-with col7:
-    st.image(resize_image(image7), use_column_width=True, caption='Image 7')
-with col8:
     st.image(resize_image(image8), use_column_width=True, caption='Image 8')
-    
+
 # Description
 st.write("Explore the beauty of Chettanahalli, a serene village nestled in the heart of K R Pete, Mandya. "
          "Immerse yourself in the rich culture, heritage, and natural landscapes that define this picturesque "
